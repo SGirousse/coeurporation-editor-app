329d73e151aa7a80fcad34cc33f9eafcb372472f
Siméon <simeon.girousse@gmail.com>
2025-02-27 07:58:08 +0100

    info des cartes standard fixé en mm

--- src/lib/Cards/Card.svelte
+++ src/lib/Cards/Card.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
     import { writable } from "svelte/store";
-    import { PrinterOutline, TrashBinOutline } from "flowbite-svelte-icons";
+    import {
+        PrinterOutline,
+        TrashBinOutline,
+        BatteryOutline,
+        DollarOutline,
+    } from "flowbite-svelte-icons";
     import type { CardType } from "$lib/index.ts";
+
     export let card: CardType;
     export let onDeleteAccessor;
     let isEditing = writable(false);
@@ -35,18 +41,26 @@
     function stopEditing() {
         editingField.set(null);
     }
+
+    $: gradeColor = card.grade
+        ? card.grade === "A"
+            ? "bg-green-500"
+            : card.grade === "B"
+              ? "bg-blue-500"
+              : card.grade === "C"
+                ? "bg-yellow-500"
+                : card.grade === "D"
+                  ? "bg-orange-500"
+                  : card.grade === "Stg"
+                    ? "bg-purple-500"
+                    : "bg-gray-500"
+        : "";
 </script>
 
 <div
-    class="group relative flex h-[88.9mm] w-[63.5mm] flex-col rounded-lg border p-1 shadow-lg rsccard"
+    class="group relative flex h-[88.9mm] w-[63.5mm] flex-col rounded-lg border p-1 shadow-lg standard-card"
 >
     <div class="absolute top-2 right-2 flex space-x-2 z-10">
-        <button
-            on:click={print}
-            class="rounded bg-gray-200 p-1 text-gray-700 opacity-0 transition-opacity group-hover:opacity-100"
-        >
-            <PrinterOutline class="text-amber-300" />
-        </button>
         <button
             on:click={deleteCard}
             class="rounded bg-gray-200 p-1 text-gray-700 opacity-0 transition-opacity group-hover:opacity-100"
@@ -56,25 +70,25 @@
     </div>
 
     <div class="flex-grow cardcontent p-1">
-        <div class="flex flex-col space-y-2">
+        <div class="flex flex-col space-y-1">
             {#if $editingField === "title"}
                 <input
                     type="text"
                     bind:value={card.title}
-                    class="border-none p-0 text-lg font-bold focus:outline-none"
+                    class="edited border-none p-0 text-lg font-bold focus:outline-none"
                     on:blur={stopEditing}
                     on:keydown={(e) => e.key === "Enter" && stopEditing()}
                 />
             {:else}
                 <div
-                    class="h-[10%] truncate text-lg font-bold"
+                    class="h-[8mm] truncate text-lg font-bold"
                     on:click={() => startEditing("title")}
                 >
                     {card.title}
                 </div>
             {/if}
 
-            <div class="relative h-[40%]">
+            <div class="relative h-[30mm]">
                 <img
                     src={card.illustration}
                     alt="Card Illustration"
@@ -86,48 +100,131 @@
                     on:change={handleImageChange}
                     class="absolute inset-0 opacity-0 cursor-pointer"
                 />
+
+                {#if $editingField === "grade"}
+                    <input
+                        type="text"
+                        bind:value={card.grade}
+                        class="edited absolute -top-3 -right-1 {gradeColor} flex h-8 w-8 items-center justify-center rounded-full text-white text-center focus:outline-none"
+                    />
+                {:else}
+                    <div
+                        class="absolute -top-3 -right-1 {gradeColor} flex h-8 w-8 items-center justify-center rounded-full text-white"
+                        on:click={() => startEditing("grade")}
+                    >
+                        {card.grade}
+                    </div>
+                {/if}
             </div>
 
-            {#if $editingField === "lore"}
-                <textarea
-                    bind:value={card.lore}
-                    class="border-none p-0 text-xs leading-tight italic focus:outline-none"
-                    on:blur={stopEditing}
-                    on:keydown={(e) => e.key === "Enter" && stopEditing()}
-                ></textarea>
-            {:else}
-                <div
-                    class="truncate-2-lines h-[10%] text-xs italic"
-                    on:click={() => startEditing("lore")}
-                >
-                    {card.lore}
-                </div>
-            {/if}
+            <div class="relative h-[9mm]">
+                {#if $editingField === "lore"}
+                    <textarea
+                        rows="2"
+                        bind:value={card.lore}
+                        class="edited border-none p-0 text-xs leading-tight italic focus:outline-none"
+                        on:blur={stopEditing}
+                        on:keydown={(e) => e.key === "Enter" && stopEditing()}
+                    ></textarea>
+                {:else}
+                    <div
+                        class="truncate-2-lines text-xs italic"
+                        on:click={() => startEditing("lore")}
+                    >
+                        {card.lore}
+                    </div>
+                {/if}
+            </div>
 
-            {#if $editingField === "effect"}
-                <textarea
-                    bind:value={card.effect}
-                    class="h-[30%] border-none p-0 text-sm leading-tight focus:outline-none"
-                    on:blur={stopEditing}
-                    on:keydown={(e) => e.key === "Enter" && stopEditing()}
-                ></textarea>
-            {:else}
-                <div
-                    class="h-[30%] overflow-hidden text-sm leading-tight text-ellipsis effect_area"
-                    on:click={() => startEditing("effect")}
-                >
-                    {card.effect}
-                </div>
-            {/if}
+            <div class="relative h-[24mm] bg-amber-200">
+                {#if $editingField === "effect"}
+                    <textarea
+                        bind:value={card.effect}
+                        class="edited border-none p-0 text-sm leading-tight focus:outline-none"
+                        on:blur={stopEditing}
+                        on:keydown={(e) => e.key === "Enter" && stopEditing()}
+                    ></textarea>
+                {:else}
+                    <div
+                        class="h-full overflow-hidden text-sm leading-tight text-ellipsis effect_area align-text-top"
+                        on:click={() => startEditing("effect")}
+                    >
+                        {card.effect}
+                    </div>
+                {/if}
+            </div>
+
+            <div class="flex h-[9mm] items-center justify-between">
+                {#if card.burnoutPoints}
+                    <div class="flex items-center w-[15mm]">
+                        <BatteryOutline />
+                        {#if $editingField === "burnoutPoints"}
+                            <input
+                                type="number"
+                                bind:value={card.burnoutPoints}
+                                class="edited border-none p-0 leading-tight focus:outline-none"
+                                on:blur={stopEditing}
+                                on:keydown={(e) =>
+                                    e.key === "Enter" && stopEditing()}
+                            />
+                        {:else}
+                            <button
+                                class="truncate-2-lines pl-1"
+                                on:click={() => startEditing("burnoutPoints")}
+                                >{card.burnoutPoints}</button
+                            >
+                        {/if}
+                    </div>
+                {/if}
+                {#if card.cost}
+                    <div class="flex items-center w-[15mm]">
+                        <DollarOutline />
+                        {#if $editingField === "cost"}
+                            <input
+                                type="number"
+                                bind:value={card.cost}
+                                class="edited border-none p-0 leading-tight focus:outline-none"
+                                on:blur={stopEditing}
+                                on:keydown={(e) =>
+                                    e.key === "Enter" && stopEditing()}
+                            />
+                        {:else}
+                            <button
+                                class="truncate-2-lines pl-1"
+                                on:click={() => startEditing("cost")}
+                                >{card.cost}</button
+                            >
+                        {/if}
+                        k
+                    </div>
+                {/if}
+            </div>
         </div>
     </div>
 </div>
 
 <style>
-    .rsccard {
+    .standard-card {
         background-color: white;
         color: black;
     }
+
+    .standard-card .edited {
+        background-color: rgb(197, 197, 197);
+    }
+
+    .standard-card button {
+        text-align: left;
+        width: 100%;
+        height: 100%;
+    }
+
+    .standard-card textarea {
+        width: 100%;
+        height: 100%;
+        resize: none;
+    }
+
     .cardcontent {
         border-radius: 5px;
         color: black;
