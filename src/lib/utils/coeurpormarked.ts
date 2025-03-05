0054ee392e947bdf6f0241de58173585307d7316
Siméon <simeon.girousse@gmail.com>
2025-03-05 11:07:59 +0100

    Support rep, base rev, opti rev, pen thresh, grade standalone et factorisation code

--- src/lib/utils/coeurpormarked.ts
+++ src/lib/utils/coeurpormarked.ts
@@ -9,9 +9,15 @@ const ICON_BATTERY = `<svg class="w-4 h-4 inline-block" aria-hidden="true" xmlns
 
 
 const ICON_HAPPYNESS = `<svg class="w-4 h-4 inline-block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.99 9H15M8.99 9H9m12 3a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM7 13c0 1 .507 2.397 1.494 3.216a5.5 5.5 0 0 0 7.022 0C16.503 15.397 17 14 17 13c0 0-1.99 1-4.995 1S7 13 7 13Z"/>
-                        </svg>`
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.99 9H15M8.99 9H9m12 3a9 9 0 1 1-18 0 9 9 0 0 1-18 0ZM7 13c0 1 .507 2.397 1.494 3.216a5.5 5.5 0 0 0 7.022 0C16.503 15.397 17 14 17 13c0 0-1.99 1-4.995 1S7 13 7 13Z"/>
+                        </svg>`;
 
+const ICON_AWARD = `<svg class="w-4 h-4 inline-block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7.171 12.906-2.153 6.411 2.672-.89 1.568 2.34 1.825-5.183m5.73-2.678 2.154 6.411-2.673-.89-1.568 2.34-1.825-5.183M9.165 4.3c.58.068 1.153-.17 1.515-.628a1.681 1.681 0 0 1 2.64 0 1.68 1.68 0 0 0 1.515.628 1.681 1.681 0 0 1 1.866 1.866c-.068.58.17 1.154.628 1.516a1.681 1.681 0 0 1 0 2.639 1.682 1.682 0 0 0-.628 1.515 1.681 1.681 0 0 1-1.866 1.866 1.681 1.681 0 0 0-1.516.628 1.681 1.681 0 0 1-2.639 0 1.681 1.681 0 0 0-1.515-.628 1.681 1.681 0 0 1-1.867-1.866 1.681 1.681 0 0 0-.627-1.515 1.681 1.681 0 0 1 0-2.64c.458-.361.696-.935.627-1.515A1.681 1.681 0 0 1 9.165 4.3ZM14 9a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                    </svg>`
+
+const COLOR_CLASS_NEGATIVE = "text-red-600";
+const COLOR_CLASS_POSITIVE = "text-green-600";
 /**
      * Interprets the markdown to some html and then replaces some variables with the game informatons.
      *
@@ -20,12 +26,18 @@ const ICON_HAPPYNESS = `<svg class="w-4 h-4 inline-block" aria-hidden="true" xml
      * - `%cardGrade%`  : `card.grade` (only Ressource Cards)
      * - `%cardBP%`     : `card.burnoutPoints`<batteryIcon> (only Ressource Cards)
      * - `%cardCost%`   : `card.cost`k€ (only Ressource Cards)
+     * - `%cardBR%`     : `card.baseRevenue`k€ (only Project Cards)
+     * - `%cardOR%`     : `card.optimalRevenue`k€ (only Project Cards)
+     * - `%cardRep%`    : `card.reputation`<reputationIcon> (only Project Cards)
+     * - `%cardPenT%`   : `card.penaltyThreshold` (only Project Cards)
      * - `%cardClient%` : `card.client` (only Project Cards)
      * 
      * Supported calculated variables :
      * - `%bp<sign>x%`       : write <sign>x<batteryIcon>
      * - `%k<sign>x%`        : write <sign>xk€
      * - `%happy<sign>x%`    : write <sign>x<happynessIcon>
+     * - `%rep<sign>x%`      : write <sign>x<reputationIcon>
+     * - `%grade<value>%`    : write <value> in the grade formatting
      *
      * @param rawText html text to be transformed.
      * @param card card data to be used for data extraction.
@@ -37,43 +49,45 @@ export async function coeurpormarked(rawText: string, card: CardType): Promise<s
         .replaceAll("%cardTitle%", card.title)
         .replaceAll(
             "%cardGrade%",
-            `<span class="inline-block ${grades.find((gradeItem) => gradeItem.value === card.grade)?.color} compact-select flex h-4 w-4 rounded-full text-white text-center focus:outline-none p-0 m-0">${card.grade}</span>`,
+            replaceGrade(card.grade)
         )
         .replaceAll("%cardBP%", `${card.burnoutPoints}${ICON_BATTERY}`)
         .replaceAll("%cardCost%", `${card.cost}k€`)
-        .replaceAll("%cardClient%", `${clients.find((client) => client.id === card.client)?.name}`);
+        .replaceAll("%cardBR%", `${card.baseRevenue}k€`)
+        .replaceAll("%cardOR%", `${card.optimalRevenue}k€`)
+        .replaceAll("%cardRep%", `${card.reputation}${ICON_AWARD}`)
+        .replaceAll("%cardPenT%", `${card.penaltyThreshold}`)
+        .replaceAll("%cardClient%", `${clients.find((client) => client.id === card.client)?.name}`)
 
-    coeurpormarkedText = coeurpormarkedText.replace(/%bp-(\d+)%/g, (match, p1) => {
-        return `<span class="text-green-600">-${p1}&nbsp;${ICON_BATTERY}</span>`;
+    coeurpormarkedText = coeurpormarkedText.replace(new RegExp(`%grade([A-Z]+)%`, "gi"), (match, p1) => {
+        return replaceGrade(p1)
     });
 
-    coeurpormarkedText = coeurpormarkedText.replace(/%bp[+](\d+)%/g, (match, p1) => {
-        return `<span class="text-red-600">+${p1}&nbsp;${ICON_BATTERY}</span>`;
-    });
-
-    coeurpormarkedText = coeurpormarkedText.replace(/%bp[_](\d+)%/g, (match, p1) => {
-        return `<span>${p1}&nbsp;${ICON_BATTERY}</span>`;
-    });
-
-    coeurpormarkedText = coeurpormarkedText.replace(/%happy-(\d+)%/g, (match, p1) => {
-        return `<span class="text-red-600">-${p1}&nbsp;${ICON_HAPPYNESS}</span>`;
-    });
-
-    coeurpormarkedText = coeurpormarkedText.replace(/%happy[+](\d+)%/g, (match, p1) => {
-        return `<span class="text-green-600">+${p1}&nbsp;${ICON_HAPPYNESS}</span>`;
-    });
-
-    coeurpormarkedText = coeurpormarkedText.replace(/%happy[_](\d+)%/g, (match, p1) => {
-        return `<span>${p1}&nbsp;${ICON_HAPPYNESS}</span>`;
-    });
-
-    coeurpormarkedText = coeurpormarkedText.replace(/%k-(\d+)%/g, (match, p1) => {
-        return `<span class="text-red-600">${p1}k€</span>`;
-    });
-
-    coeurpormarkedText = coeurpormarkedText.replace(/%k[+](\d+)%/g, (match, p1) => {
-        return `<span class="text-green-600">${p1}k€</span>`;
-    });
+    coeurpormarkedText = replaceVariable(coeurpormarkedText, "bp", ICON_BATTERY);
+    coeurpormarkedText = replaceVariable(coeurpormarkedText, "happy", ICON_HAPPYNESS);
+    coeurpormarkedText = replaceVariable(coeurpormarkedText, "k", "k€");
+    coeurpormarkedText = replaceVariable(coeurpormarkedText, "rep", ICON_AWARD);
 
     return coeurpormarkedText;
+}
+
+function replaceGrade(gradeValue: string): string {
+    return `<span class="inline-block ${grades.find((gradeItem) => gradeItem.value === gradeValue)?.color} compact-select flex h-4 w-4 rounded-full text-white text-center focus:outline-none p-0 m-0">${gradeValue}</span>`;
+}
+
+function replaceVariable(text: string, variable: string, icon: string): string {
+
+    text = text.replace(new RegExp(`%${variable}-(\\d+)%`, "g"), (match, p1) => {
+        return `<span class="${variable == "bp" ? COLOR_CLASS_POSITIVE : COLOR_CLASS_NEGATIVE}">-${p1}&nbsp;${icon}</span>`;
+    });
+
+    text = text.replace(new RegExp(`%${variable}[+](\\d+)%`, "g"), (match, p1) => {
+        return `<span class="${variable == "bp" ? COLOR_CLASS_NEGATIVE : COLOR_CLASS_POSITIVE}">+${p1}&nbsp;${icon}</span>`;
+    });
+
+    text = text.replace(new RegExp(`%${variable}[_](\\d+)%`, "g"), (match, p1) => {
+        return `<span>${p1}&nbsp;${icon}</span>`;
+    });
+
+    return text;
 }
